9992bf2a3a714c068b459711c8b8bb0197d14140
Maria Cernohouzova <maria.cernohouzova@gmail.com>
2023-10-23 11:23:47 +0200

    ukol1

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const jmeno = "Michal"
+let jmeno = prompt("Zadej jméno:")
+let prijmeni = prompt ("Zadej příjmení:")
 
+let jmenobezznaku = jmeno.trim()
+let prijmenibezznaku = prijmeni.trim ()
 
-document.body.innerHTML = "<h1>" + jmeno + "<h1>"
+let jmenokonec = jmenobezznaku.slice(0,3)
+let prijmenikonec = prijmenibezznaku.slice(0,5)
+
+const domena = "fit.cvut.cz"
+const email = jmenokonec + prijmenikonec + "@" + domena
+document.body.innerHTML += `<p>Tvuj email je: ${email}</p>`
